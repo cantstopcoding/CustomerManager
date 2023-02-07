416043a00202ecbf3f664471b208bef332bcac80
cantstopcoding <jonathan_koss@yahoo.com>
2023-02-07 13:59:56 -0500

    make id and customer to unnullable

--- src/main/java/jonathan/koss/customermanager/model/Customer.java
+++ src/main/java/jonathan/koss/customermanager/model/Customer.java
@@ -1,11 +1,20 @@
 package jonathan.koss.customermanager.model;
 
-public class Customer {
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+public class Customer implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
     private Long id;
     private String name;
     private String email;
     private String jobTitle;
     private String phone;
     private String imageUrl;
+    @Column(nullable = false)
     private String customerCode;
 }
