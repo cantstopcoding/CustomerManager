928813e8c0e87b215dc231e49afc8a82ad246be2
cantstopcoding <jonathan_koss@yahoo.com>
2023-02-07 13:49:23 -0500

    add attributes for customer model

--- src/main/java/jonathan/koss/customermanager/model/Customer.java
+++ src/main/java/jonathan/koss/customermanager/model/Customer.java
@@ -2,4 +2,10 @@ package jonathan.koss.customermanager.model;
 
 public class Customer {
     private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    private String customerCode;
 }
